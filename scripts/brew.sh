ff778b4791de72e5c0e61c9304c91ccb31dcc7e6
Alexander Nesterov <0x12ee705@gmail.com>
2022-06-07 21:09:24 +0700

    Added brew.sh

---
+++ scripts/brew.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+brew tap homebrew/cask-fonts
+brew tap adoptopenjdk/openjdk
+
+echo BREW INSTALL:
+brew install \
+git \
+bat \
+fzf \
+git-flow \
+dive \
+far2l \
+telnet \
+binwalk \
+coreutils \
+dep \
+direnv \
+findutils \
+fping \
+tmux \
+yq \
+jq \
+gpg \
+vagrant \
+qemu \
+adoptopenjdk11
